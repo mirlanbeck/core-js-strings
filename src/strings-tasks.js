6d57ac3cf1ef57b7fb8cd6eabb8085ab19ecf0b6
Mirlanbek Osmonov <mail_to_mirlan@mail.ru>
2026-09-19 21:44:19 +0600

    implement concatenateStrings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -59,8 +59,9 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  const concatResult = value1.concat(value2);
+  return concatResult;
 }
 
 /**
